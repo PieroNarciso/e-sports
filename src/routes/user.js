b8dd67d0633846a456d07b888e1cc24fb84fbd5e
sbanonf <sbanonf@gmail.com>
2021-06-18 23:03:28 -0500

    Cambios hechos en el video de hoy aaaa

---
+++ src/routes/user.js
@@ -0,0 +1,17 @@
+const { Router } = require('express');
+
+const { getUser, loginUser } = require('../controllers/user');
+const router = Router();
+
+
+router.get('/1', getUser);
+
+router.get('/login', loginUser);
+router.post('/login', loginPostUser)
+
+router.get('/user1', (req, res) => {
+    res.send('User 1');
+});
+
+
+module.exports = router;
