73537204ef0fb52be1c981f526165f1be1f85d2b
sbanonf <sbanonf@gmail.com>
2021-06-26 11:25:17 -0500

    Agregada verificacion

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,4 +1,58 @@
+const { Usuario, Equipo } = require("../models");
+const models= require("../models");
+const usuario=models.Usuario;
+const equipo= models.Equipo;
 
+const verificacion = (nombre,equipo) =>{
+    let verificado;
+    Usuario.findAll({
+        where:{
+            correo:correo,
+            rol:"lider"
+        },
+        attributes:['correo'],
+    }) 
+    .then(lusr => { 
+        console.log(lusr)
+        if(lusr.length>0){
+            verificado=false;
+        }else{
+            verificado=true;
+        }
+
+    })
+    
+    //.then((lusr)=>{
+        //lusr.forEach(element =>{
+            //jsonObject= element.get({raw:true})
+           // console.log(jsonObject)
+            /*if(jsonObject == "nombre_completo: "+nombre){
+                console.log("si existe")
+                return false
+            }else{
+                return true
+            }*/
+
+        //})
+
+    //})
+    Equipo.findAll({
+        where:{
+            nombre:equipo
+
+        }
+
+    })
+    .then((lequipo)=>{
+        if(lequipo.length>0){
+            verificado=false;
+        }else{
+            verificado=true;
+        }    
+
+    })
+    return verificado;
+}
 module.exports = {
     getUser: (req, res) => {
         res.send('User');    
@@ -22,8 +76,18 @@ module.exports = {
             correo=req.body.correo;
             contrasena=req.body.contrasena;
             nombreequipo=req.body.equipo;
-            console.log("nombre"+nombre+"correo"+correo+"contraseña: "+contrasena+"nombre equipo"+nombreequipo)
-            res.redirect("/")
+            const usr={nombre_completo: nombre, correo: correo, contraseña: contrasena, rol: 'lider'}
+            const equipo={ nombre: nombreequipo, lista_integrantes: ["Gorila","Jaguar","Venado"]}
+            if(verificacion(correo, nombreequipo) == true){
+                Usuario.create(usr);
+                Equipo.create(equipo);
+                console.log("si es")
+                res.redirect("/")
+            }else{
+                console.log("a")
+                estado=false;
+                res.render("registro",{estado})
+            }
         }
         catch(e){
             console.log(e)
@@ -45,7 +109,32 @@ module.exports = {
     },
     perfilActualizarPostUser:(req,res)=>{
         try{
+            nuevo_nombre=req.body.nombre;
+            nuevo_correo=req.body.correo;
+            nuevo_nombreequipo=req.body.equipo;
+            if(verificacion(nuevo_correo, nuevo_nombreequipo) == true){
+                Usuario.update(
+                    {
+                    nombre_completo: nuevo_nombre,
+                    correo: nuevo_correo,
+                    },{
+                        where:{
+                            
+                        }
+                    }
+                )
+                Equipo.update({
+                    nombre: nuevo_nombreequipo
+                },{
+                    where:{
 
+                    } 
+                });
+                res.redirect("/")
+            }else{
+                estado=false;
+                res.render("registro",{estado})
+            }
         }
         catch{
             estado=false;
