1637dfc651a279f8829511537eebdf8c65ae846b
Piero Narciso <pieronarciso988@gmail.com>
2021-06-27 15:20:37 -0500

    Doc routes

--- src/routes/torneo.js
+++ src/routes/torneo.js
@@ -6,6 +6,9 @@ const router = Router();
 
 
 router.get('/', getTorneos);
+/**
+  * Renderiza la lista de equipos del respectivo torneo `id`
+  */
 router.get('/:id', getTorneoById);
 
 module.exports = router;
