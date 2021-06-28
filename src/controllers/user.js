c01f18bff735d9326ca76f3d003b1788c7ccb56d
sbanonf <sbanonf@gmail.com>
2021-06-28 00:43:19 -0500

    Finalizada 4 historias, falta encriptar y convertir atributos Integrantes de Equipo  a string

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,12 +1,13 @@
 
 const { Usuario, Equipo } = require("../models");
 const sequelize = require("sequelize")
-const op= sequelize.Op
-const models= require("../models");
-const usuario=models.Usuario;
-const equipo= models.Equipo;
+const op = sequelize.Op
+const models = require("../models");
+const usuario = models.Usuario;
+const equipo = models.Equipo;
 const bcrypt = require('bcrypt');
 const { authParticipanteLider } = require("../middlewares/auth");
+const { HostNotReachableError } = require("sequelize");
 
 
 /*const verificacion = (nombre,equipo) =>{
@@ -39,212 +40,282 @@ const { authParticipanteLider } = require("../middlewares/auth");
                 return true
             }*/
 
-        //})
+//})
 
-    //})
-    /*Equipo.findAll({
-        where:{
-            nombre:equipo
+//})
+/*Equipo.findAll({
+    where:{
+        nombre:equipo
 
-        }
+    }
 
-    })
-    .then((lequipo)=>{
-        if(lequipo.length>0){
-            verificado=false;
-        }else{
-            verificado=true;
-        }
-    
+})
+.then((lequipo)=>{
+    if(lequipo.length>0){
+        verificado=false;
+    }else{
+        verificado=true;
+    }
+ 
 
-    })
-    return verificado;
+})
+return verificado;
 }*/
 module.exports = {
-  /**
-  * @param {import('express').Request} req
-  * @param {import('express').Response} res
-  *
-  * Renderiza la pantalla del `Login` para diferentes usuarios
-  * `admin | org | lider`
-  */
-  getLoginUser: (_, res) => {
-    res.render('login', { msg: '' });
-  },
-
-  /**
-  * @param {import('express').Request} req
-  * @param {import('express').Response & import('express-session').SessionData} res
-  *
-  * Verifica la informacion otorgada por el usuario al hacer login
-  */
-  postLoginUser: async (req, res) => {
-    const { email, password } = req.body;
-    const msg = 'Email o contraseña incorrecto';
-    try {
-      const usr = await Usuario.findOne({
-        where: { correo: email }
-      });
-      // Usuario existe
-      if (usr) {
-        const isValid = await bcrypt.compare(password, usr.password);
-        // Es valido, se loguea
-        if (isValid) {
-          req.session.userId = usr.id;
-          req.session.rol = usr.rol;
-          
-          return res.redirect('/');
-        }
-        // No es valido
-        return res.render('login', { msg });
-      }
-      // Usuario no existe
-      return res.render('login', { msg });
-    } catch(err) {
-    }
-  },
-    //Get de la visa Registro
-    registroUser: (req,res) => {
-        const estado = true; //Si estado == true, no se mostrará el mensaje error en la pagina.
-        res.render("registro",{estado});
+    /**
+    * @param {import('express').Request} req
+    * @param {import('express').Response} res
+    *
+    * Renderiza la pantalla del `Login` para diferentes usuarios
+    * `admin | org | lider`
+    */
+    getLoginUser: (_, res) => {
+        res.render('login', { msg: '' });
     },
-    
-    //Post de la vista Registro
-    registroPostUser:(req,res) => {
-        estado=true; //Si estado == true, no se mostrará el mensaje error en la pagina.
-        usuario.findAll({ //Encuentra todos los usuarios, donde el correo del registro sea igual.
-            where:{correo: req.body.correo}
-        })
-        .then( (lusur) => { 
-            console.log(lusur)
-            if(lusur.length > 0 ){//si el tamaño es mayor que 0 entonces si hay usuario.
-                console.log("entree") //Debug 
-                const estado=false;//ERROR
-                res.render("registro",{estado}) //Se envia para mostrar con el error
+
+    /**
+    * @param {import('express').Request} req
+    * @param {import('express').Response & import('express-session').SessionData} res
+    *
+    * Verifica la informacion otorgada por el usuario al hacer login
+    */
+    postLoginUser: async (req, res) => {
+        const { email, password } = req.body;
+        const msg = 'Email o contraseña incorrecto';
+        try {
+            const usr = await Usuario.findOne({
+                where: { correo: email }
+            });
+            // Usuario existe
+            if (usr) {
+                const isValid = await bcrypt.compare(password, usr.password);
+                // Es valido, se loguea
+                if (isValid) {
+                    req.session.userId = usr.id;
+                    req.session.rol = usr.rol;
+
+                    return res.redirect('/');
+                }
+                // No es valido
+                return res.render('login', { msg });
             }
-            else{
-                equipo.findAll({ //Encuentra todos los equipos donde el nombre a crear sea el mismo
-                    where: {
-                        nombre: req.body.equipo
-                    } 
-                })
-                .then( (lequip) =>{
-                    console.log(lequip)
-                    if(lequip.length > 0){ //si el tamaño es mayor que 0 entonces si hay equipo.
-                        console.log(lequip)
-                        const estado= false; //ERROR
-                        res.render("registro",{estado}) //Se envia para mostrar con el error
-                    }else{
-                        Usuario.create({ //se crea usuario
-                            nombre_completo: req.body.nombre,
-                            correo: req.body.correo,
-                            password: req.body.contrasena,
-                            rol:'lider'
-                        })
-                        .then((rpta) =>{ //se crea equipo
-                            Equipo.create({
-                                nombre: req.body.equipo,
-                                lista_integrantes: ['Gorila','leon','perro'],
-                                lider_id: rpta.id //Se le asigna el id del participante lider creado al euqipo
-                            })
-                            .then((rpta) =>{
-                                res.redirect('/') // Se redirecciona.
-                            }).catch(error =>{
-                                res.sen(500).send(error)
-                            })
-                        }).catch(error =>{
+            // Usuario no existe
+            return res.render('login', { msg });
+        } catch (err) {
+        }
+    },
+    //Get de la visa Registro
+    registroUser: (req, res) => {
+        const estado = true; //Si estado == true, no se mostrará el mensaje error en la pagina.
+        res.render("registro", { estado:estado });
+    },
+
+    //Post de la vista Registro
+    registroPostUser: (req, res) => {
+        estado = true; //Si estado == true, no se mostrará el mensaje error en la pagina.
+        usuario.findAll({ //Encuentra todos los usuarios, donde el correo del registro sea igual.
+            where: { correo: req.body.correo }
+        })
+            .then((lusur) => {
+                console.log(lusur)
+                if (lusur.length > 0) {//si el tamaño es mayor que 0 entonces si hay usuario.
+                    console.log("entree") //Debug 
+                    const estado = false;//ERROR
+                    res.render("registro", { estado: estado }) //Se envia para mostrar con el error
+                }
+                else {
+                    equipo.findAll({ //Encuentra todos los equipos donde el nombre a crear sea el mismo
+                        where: {
+                            nombre: req.body.equipo
+                        }
+                    })
+                        .then((lequip) => {
+                            console.log(lequip)
+                            if (lequip.length > 0) { //si el tamaño es mayor que 0 entonces si hay equipo.
+                                console.log(lequip)
+                                const estado = false; //ERROR
+                                res.render("registro", { estado }) //Se envia para mostrar con el error
+                            } else {
+                                Usuario.create({ //se crea usuario
+                                    nombre_completo: req.body.nombre,
+                                    correo: req.body.correo,
+                                    password: req.body.contrasena,
+                                    rol: 'lider'
+                                })
+                                    .then((rpta) => { //se crea equipo
+                                        Equipo.create({
+                                            nombre: req.body.equipo,
+                                            lista_integrantes: ['Gorila', 'leon', 'perro'],
+                                            lider_id: rpta.id //Se le asigna el id del participante lider creado al euqipo
+                                        })
+                                            .then((rpta) => {
+                                                res.redirect('/') // Se redirecciona.
+                                            }).catch(error => {
+                                                res.sen(500).send(error)
+                                            })
+                                    }).catch(error => {
+                                        res.sen(500).send(error)
+                                    })
+                            }
+                        }).catch(error => {
                             res.sen(500).send(error)
                         })
-                    }
-                }).catch(error =>{
-                    res.sen(500).send(error)
-                })     
-            }
-        }).catch(error =>{
-            res.sen(500).send(error)
-        })
+                }
+            }).catch(error => {
+                res.sen(500).send(error)
+            })
     },
     //GET DEL PERFIL DEL LIDER
-    perfilUser:(req,res)=>{
-        
-        res.render("perfilLider",{nombre: "Pepe",correo:"pepe@gmail.com",equipo:"Gatos"
-    })
+    perfilUser: (req, res) => {
+        usuario.findAll({
+            where:
+                { id: req.session.userId }
+        })
+            .then(usuario => {
+                console.log(usuario)
+                console.log(usuario[0].id)
+                equipo.findAll({ where: { lider_id: usuario[0].id } })
+                    .then(equipo => {
+                        res.render("perfilLider", { nombre: usuario[0].nombre_completo, correo: usuario[0].correo, equipo: equipo[0].nombre })
+                    })
+            })
+        //res.render("perfilLider",{nombre: "Pepe",correo:"pepe@gmail.com",equipo:"Gatos"
+        // })
     },
     //POST DEL PERFIL DEL LIDER
-    perfilPostUser:(req,res) =>{
-        
+    perfilPostUser: (req, res) => {
+
     },
     //GET DEL ACTUALIZAR DEL LIDER
-    perfilActualizarUser:(req,res)=>{
-        const estado=true; //Si estado == true, no se mostrará el mensaje error en la pagina.
-        res.render("perfilLiderActualizar",{estado})
+    perfilActualizarUser: (req, res) => {
+        usuario.findAll({
+            where:
+                { id: req.session.userId }
+        })
+            .then(usuario => {
+                console.log(usuario)
+                console.log(usuario[0].id)
+                const estado = true; //Si estado == true, no se mostrará el mensaje error en la pagina.
+                res.render("perfilLiderActualizar", { estado: estado, u: usuario[0]})
+            })
+        
     },
     //POST DEL ACTUALIZAR DEL LIDER
-    perfilActualizarPostUser:(req,res)=>{
-
-
-
-        /*try{
-            nuevo_nombre=req.body.nombre;
-            nuevo_correo=req.body.correo;
-            nuevo_nombreequipo=req.body.equipo;
-            if(verificacion(nuevo_correo, nuevo_nombreequipo) == true){
-                Usuario.update(
-                    {
-                    nombre_completo: nuevo_nombre,
-                    correo: nuevo_correo,
-                    },{
-                        where:{
-                            
+    perfilActualizarPostUser: (req, res) => {
+        console.log("hola")
+        usuario.findByPk(req.session.userId).then(usuario_conectado =>{
+            usuario.findAll({
+                where: {
+                    correo: req.body.correo,
+                    id: {[op.ne] :[req.session.userId]}
+                }
+            }).then(usuarios => {
+                if (usuarios.length > 0) {
+                    console.log("a")
+                    var estado = false; //Si estado == true, no se mostrará el mensaje error en la pagina.
+                    res.render("perfilLiderActualizar", { estado: estado, u: usuario_conectado[0]})
+                } else {
+                    usuario.update(
+                        {
+                            nombre_completo: req.body.nombre,
+                            correo: req.body.correo,
+                            password: req.body.contrasena
                         }
-                    }
-                )
-                Equipo.update({
-                    nombre: nuevo_nombreequipo
-                },{
-                    where:{
+                        ,
+                        {
+                            where:
+                            {
+                                id: req.session.userId,
+                            }
+                        }
+                    ).then(usuario => {
+                        console.log(usuario)
+                        res.redirect('/')
+                    }).catch(error => {
+                        res.send(500).send(error)
+                    })
+    
+                }
+            }).catch(error => {
+                res.send(500).send(error)
+            })
 
-                    } 
-                });
-                res.redirect("/")
-            }else{
-                estado=false;
-                res.render("registro",{estado})
-            }
-        }
-        catch{
-            estado=false;
-            res.render("registro",{estado})
 
-        }
-        res.redirect("/")*/
+
+        })
+        
     },
     //GET DEL EQUIPO
-    equipoUser:(req,res)=>{
-        res.render("perfilEquipo",{equipo: "Gatos", integrantes:"Pepe,Juan,Pikachu"})
+    equipoUser: (req, res) => {
+        equipo.findAll({where:{
+            lider_id: req.session.userId
+        }})
+        .then(equipos=> {
+            res.render("perfilEquipo",{equipo: equipos[0].nombre, integrantes: equipos[0].lista_integrantes})
+        })
 
     },
     //POST DEL EQUIPO
-    equipoPostUser:(req,res) =>{
+    equipoPostUser: (req, res) => {
 
     },
     //GET DEL EQUIPO ACTUALIZAR
-    equipoActualizarUser:(req,res)=>{
-        const estado=true;
-        res.render("perfilEquipoActualizar",{estado})
+    equipoActualizarUser: (req, res) => {
+        equipo.findAll({where:{
+            lider_id: req.session.userId
+        }}).then(equipo_usuario =>{
+            const estado = true;
+            res.render("perfilEquipoActualizar", { estado: estado, u:equipo_usuario[0]})
+        })
+
     },
     //POST DEL EQUIPO ACTUALIZAR
-    equipoActualizarPostUser:(req,res)=>{
-        try{
-
-        }
-        catch{
-          estado=false;
-          res.render("registro",{estado})
-        }
-        res.redirect("/")
-      }
+    equipoActualizarPostUser: (req, res) => {
+        equipo.findAll({where:{
+            lider_id: req.session.userId
+        }}).then(equipo_usuario =>{
+            equipo.findAll({
+                where: {
+                    nombre: req.body.nombre,
+                    lider_id: {[op.ne] :[req.session.userId]}
+                }
+            }).then(equipos_encontrados => {
+                if (equipos_encontrados.length > 0) {
+                    var estado = false; //Si estado == true, no se mostrará el mensaje error en la pagina.
+                    res.render("perfilEquipoActualizar", { estado: estado, u: equipo_usuario[0]})
+                } else {
+                    console.log("AAAAAAAAAAAAA")
+                    var integrantes= req.body.integrante
+                    var lista_integrantes=[integrantes]
+                    equipo.update(
+                        {
+                            nombre: req.body.nombre,
+                            lista_integrantes: lista_integrantes
+                        }
+                        ,
+                        {
+                            where:
+                            {
+                                lider_id: req.session.userId,
+                            }
+                        }
+                    ).then(rpta => {
+                        console.log(rpta)
+                        res.redirect('/')
+                    }).catch(error => {
+                        res.send(500).send(error)
+                    })
     
-    
+                }
+            }).catch(error => {
+                res.send(500).send(error)
+            })
+        })
+        .catch(error=>{
+            res.send(500).send(error)
+        })
     }
+
+
+}
 
