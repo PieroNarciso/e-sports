53c1ce091c0a0fa6888b575b17d2bf1947e8eb7a
Piero Narciso <pieronarciso988@gmail.com>
2021-07-05 20:52:47 -0500

    Prod database_url

--- src/db.js
+++ src/db.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize');
 
-const { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USERNAME, DATABASE_URI } = require('./config/env');
+const { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USERNAME, DATABASE_URL } = require('./config/env');
 
 
-const sequelizeOpts = DATABASE_URI? DATABASE_URI : {
+const sequelizeOpts = DATABASE_URL? DATABASE_URL : {
   username: DB_USERNAME,
   host: DB_HOST,
   password: DB_PASSWORD,
